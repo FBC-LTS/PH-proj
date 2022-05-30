3e4b1a4c265d732c13ff0f4acb5099b8d4d6d465
FBC-LTS <101365427+FBC-LTS@users.noreply.github.com>
2022-05-30 13:59:20 -0300

    STD/MultipleSheet
    removed unnecessary classes
    multiple sheet create

---
+++ RandonSheetGenerator.py
@@ -0,0 +1,6 @@
+import Sheet
+
+def all_r_sheet():
+    #gera uma ficha com valores aleatorios
+    #generates a random character sheet
+    pass
--- Sheet.py
+++ Sheet.py
@@ -1,29 +1,93 @@
 
-from typing_extensions import Self
-
-class Personagem:
-    def __init__(self, name, level, race, stats, armor, damage, moves) -> None:
-        self.__nome = name
-        self.__lv = level
-        self.__raca = race
+class Character:
+    def __init__(self, name='', race={}, stats={}, armor=0, damage=0):
+        self.__name = name
+        self.__race = race
         self.__stats = stats
         self.__armor = armor
         self.__damage = damage
-        self.__moves = moves
+    
+    def get_name(self):
+        return self.__name
+
+    def get_race(self):
+        return self.__race
+    
+    def get_stats(self):
+        return self.__stats
+    
+    def get_armor(self):
+        return self.__armor
+    
+    def get_damage(self):
+        return self.__damage
+    
+    def armor_setter(self, armor):
+        self.__armor = armor
+    
+    def damage_setter(self, damage):
+        self.__damage = damage
+        
+    def stats_setter(self, atr, value):
+        self.__stats[atr] = value
     
 
-class PLevel:
-    def __init__(Self, lvl, xp=0 ) -> None:
-        Self.__lvl = lvl
-        Self.__xp = xp
+class CPlayer(Character):
+    def __init__(self, name='', race={}, stats={}, armor=0, damage=0, lvl=1, xp=0, moves={}):
+        super().__init__(name, race, stats, armor, damage)
+        self.__lvl = lvl
+        self.__xp = xp
+        self.__moves = moves
+
+    def get_lvl(self):
+        return self.__lvl
+
+    def get_xp(self):
+        return self.__xp
+    
+    def get_moves(self):
+        return self.__moves
+
+    def get_armor(self):
+        return super().get_armor()
+    
+    def get_damage(self):
+        return super().get_damage()
+    
+    def get_name(self):
+        return super().get_name()
+    
+    def get_race(self):
+        return super().get_race()
+    
+    def get_stats(self):
+        return super().get_stats()
+
+    def xp_setter(self, xp):
+        self.__xp += xp
+        to_lvlup = self.__lvl + 8
+        if self.__xp >= to_lvlup:
+            self.__xp -= to_lvlup
+            self.__lvl += 1
+    
+    def newmove_setter(self, move):
+        self.__moves.add(move)
 
 
-class PStats(Self):
-    def __init__(Self, stats) -> None:
-        Self.__stats = stats
 
 
-class PMoves(Self):
-    def __init__(self, name, desc) -> None:
-        self.__name = name
-        self.__desc = desc
+class MultipleSheetCreator:
+    def __init__(self):
+        self.__sheet_list = []
+    
+    def create_sheet(self, name, race, stats, armor, damage, player= False):
+        char = Character(name, race, stats, armor, damage)
+        if player:
+            player_sheet = CPlayer(name, race, stats, armor, damage, 1, 0, {})
+            self.__sheet_list.append(player_sheet)
+            return player_sheet
+        self.__sheet_list.append(char)
+        return char
+
+    def get_sheet_list(self):
+        return self.__sheet_list
