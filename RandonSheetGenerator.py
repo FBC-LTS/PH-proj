a950dd29eba4bc0b96860548164403604f4d4de0
FBC-LTS <101365427+FBC-LTS@users.noreply.github.com>
2022-06-02 18:23:02 -0300

    Character & new class(attributes)
    Removed unnecessary class "MultipleSheetCreator" for now.
    Upgrade on class Character, class "Cplayer" added on it.
    New class "CharAttributes" to add value for each attribute and calculate all modifiers.

--- RandonSheetGenerator.py
+++ RandonSheetGenerator.py
@@ -1,6 +1,30 @@
 import Sheet
+import Sheet 
+from random import randint  as rv
+
+def randon_class():
+    x=rv(0, 9)
+    match x:
+        case 0 :
+            return 'Barbarian'
+        case 1 :
+            return 'Bard'
+        case 2 :
+            return 'Cleric'
+        case 4 :
+            return 'Druid'
+        case 5 :
+            return 'Fighter'
+        case 6 :
+            return 'Paladin'
+        case 7 :
+            return 'Ranger'
+        case 8 :
+            return 'Thief'
+        case 9 :
+            return 'Wizard'
 
 def all_r_sheet():
     #gera uma ficha com valores aleatorios
     #generates a random character sheet
-    pass
+    pass    pass
--- Sheet.py
+++ Sheet.py
@@ -1,43 +1,112 @@
+from random import randint  as rv
+from secrets import choice
+
+def between(val, mini, maxi):
+    # Value is between mini and maxi?
+    if val >= mini and val <= maxi:
+        return True
+    else:
+        return False
+
+
+#class CharAttributes, here will be added value for each attribute and calculated all modifiers
+class CharAttributes:
+    def __init__(self, strength=0, dexterity=0, constitution=0, intelligence=0, wisdom=0, charisma=0) -> None:
+        self.__std_atr = [8, 9, 12, 13, 15, 16]
+        self.__str = strength
+        self.__dex = dexterity
+        self.__con = constitution
+        self.__int = intelligence
+        self.__wis = wisdom
+        self.__cha = charisma
+        self.__mod ={
+            'str': 0, 
+            'dex': 0, 
+            'con': 0, 
+            'int': 0, 
+            'wis': 0, 
+            'cha': 0
+        }
+        self.__auto_upmods()
+
+    def get_all_atts(self):
+        atts = {
+            'str': self.__str, 
+            'dex': self.__dex, 
+            'con': self.__con, 
+            'int': self.__int, 
+            'wis': self.__wis, 
+            'cha': self.__cha
+        }
+        return atts
+    
+    def __auto_upmods(self):
+        # Calculates all modifiers
+        sts_list = [self.__str, self.__dex, self.__con, self.__int, self.__wis, self.__cha]
+        mod_list = []
+        for i in sts_list:
+            if between(i, 1, 3):
+                mod_list.append(-3)
+            elif between(i, 4, 5):
+                mod_list.append(-2)
+            elif between(i, 6, 8):
+                mod_list.append(-1)
+            elif between(i, 9, 12):
+                mod_list.append(0)
+            elif between(i, 13, 15):
+                mod_list.append(1)
+            elif between(i, 16, 17):
+                mod_list.append(2)
+            elif i >= 18:
+                mod_list.append(3)
+            else:
+                mod_list.append(-100)
+
+        self.__mod.update({        
+            'str': mod_list[0], 
+            'dex': mod_list[1], 
+            'con': mod_list[2], 
+            'int': mod_list[3], 
+            'wis': mod_list[4], 
+            'cha': mod_list[5]}
+            )
+    
+    def choice_randon_setter(self):
+        # All attributes will receive the values [8, 9, 12, 13, 15, 16] randomly
+        self.__str = choice(self.__std_atr)
+        self.__dex = choice(self.__std_atr)
+        self.__con = choice(self.__std_atr)
+        self.__int = choice(self.__std_atr)
+        self.__wis = choice(self.__std_atr)
+        self.__cha = choice(self.__std_atr)
+        self.__auto_upmods()
+    
+    def full_randon_setter(self, min=0, max=18): 
+        # All attributes will receive random values
+        self.__str = rv(min, max)
+        self.__dex = rv(min, max)
+        self.__con = rv(min, max)
+        self.__int = rv(min, max)
+        self.__wis = rv(min, max)
+        self.__cha = rv(min, max)
+        self.__auto_upmods()
+    
+
 
 class Character:
-    def __init__(self, name='', race={}, stats={}, armor=0, damage=0):
-        self.__name = name
-        self.__race = race
-        self.__stats = stats
-        self.__armor = armor
-        self.__damage = damage
-    
-    def get_name(self):
-        return self.__name
-
-    def get_race(self):
-        return self.__race
-    
-    def get_stats(self):
-        return self.__stats
-    
-    def get_armor(self):
-        return self.__armor
-    
-    def get_damage(self):
-        return self.__damage
-    
-    def armor_setter(self, armor):
-        self.__armor = armor
-    
-    def damage_setter(self, damage):
-        self.__damage = damage
-        
-    def stats_setter(self, atr, value):
-        self.__stats[atr] = value
-    
-
-class CPlayer(Character):
-    def __init__(self, name='', race={}, stats={}, armor=0, damage=0, lvl=1, xp=0, moves={}):
-        super().__init__(name, race, stats, armor, damage)
-        self.__lvl = lvl
-        self.__xp = xp
-        self.__moves = moves
+    def __init__(self, atts):
+        self.__lvl = 0
+        self.__xp = 0
+        self.__armor = 0
+        self.__name = 'name'
+        self.__race = 'race'
+        self.__atts = atts
+        self.__moves = {}
+        self.__ch_class = 'class'
+        # Possible classes according to race:
+        self.__posclass = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Paladin', 'Ranger', 'Thief', 'Wizard'] 
+        # Possible reces according to class:
+        self.__posrace = ['HUMAN', 'ELF', 'DWARF', 'HALFLING'] 
 
     def get_lvl(self):
         return self.__lvl
@@ -48,20 +117,26 @@ class CPlayer(Character):
     def get_moves(self):
         return self.__moves
 
-    def get_armor(self):
-        return super().get_armor()
-    
-    def get_damage(self):
-        return super().get_damage()
-    
     def get_name(self):
-        return super().get_name()
-    
+        return self.__name
+
     def get_race(self):
-        return super().get_race()
+        return self.__race
     
-    def get_stats(self):
-        return super().get_stats()
+    def get_atts(self):
+        return self.__atts
+    
+    def get_armor(self):
+        return self.__armor
+    
+    def race_setter(self, race):
+        self.__race = race
+
+    def name_setter(self, name):
+        self.__name = name
+
+    def armor_setter(self, armor):
+        self.__armor = armor
 
     def xp_setter(self, xp):
         self.__xp += xp
@@ -70,24 +145,13 @@ class CPlayer(Character):
             self.__xp -= to_lvlup
             self.__lvl += 1
     
-    def newmove_setter(self, move):
-        self.__moves.add(move)
-
-
-
-
-class MultipleSheetCreator:
-    def __init__(self):
-        self.__sheet_list = []
+    def newmove_setter(self, move_name, move_desc):
+        self.__moves[move_name] = move_desc
     
-    def create_sheet(self, name, race, stats, armor, damage, player= False):
-        char = Character(name, race, stats, armor, damage)
-        if player:
-            player_sheet = CPlayer(name, race, stats, armor, damage, 1, 0, {})
-            self.__sheet_list.append(player_sheet)
-            return player_sheet
-        self.__sheet_list.append(char)
-        return char
-
-    def get_sheet_list(self):
-        return self.__sheet_list
+    def class_setter(self, cclass): #$$
+        if cclass.upper() in self.__posclass:
+            self.__ch_class = cclass
+    
+    def race_setter(self, race): #$$
+        if race.upper() in self.__posrace:
+            self.__race = race
